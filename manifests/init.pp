7d9288b45abd02c61d79e1aeb5adb4dd8460cab5
Kyle Oliveira <kcoliveira@ucdavis.edu>
2013-11-19 10:09:19 -0500

    Make PERLBREW_ROOT a variable
    Made some modifications to manifests/init.pp that should make
    it easy to set the desired $PERLBREW_ROOT without modifying
    the whole class every time.
    
    Resolves gh-1.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,8 @@
 #
 # === Parameters
 #
-# None.
+# [*perlbrew_root*]
+#   Specifies the $PERLBREW_ROOT to install Perlbrew to. Set to /opt/perlbrew by default.
 #
 # === Variables
 #
@@ -26,19 +27,19 @@
 # Copyright 2013 Mike Doherty, unless otherwise noted.
 #
 
-class perlbrew {
+class perlbrew($perlbrew_root = "/opt/perlbrew") {
     package { 'curl': ensure => installed }
     package { 'bash': ensure => installed }
     exec { 'install perlbrew':
         command     => '/usr/bin/curl -L http://install.perlbrew.pl | /bin/bash',
         require     => [ Package['curl'], Package['bash'] ],
-        creates     => '/opt/perlbrew/bin/perlbrew',
-        environment => [ 'PERLBREW_ROOT=/opt/perlbrew', ],
+        creates     => "${perlbrew_root}/bin/perlbrew",
+        environment => [ "PERLBREW_ROOT=${perlbrew_root}", ],
         user        => 'root',
     }
     file { '/etc/profile.d/perlbrew.sh':
         ensure  => file,
-        content => 'export PATH="$PATH:/opt/perlbrew/bin"',
+        content => "export PATH=\"\$PATH:${perlbrew_root}/bin\"",
         owner   => 'root',
         group   => 'root',
         mode    => 0644,
